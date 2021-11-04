66fb3487d87afad93da94647ee19b85883638bfc
Edgar <ecarrillo046@gmail.com>
2021-11-03 17:22:40 -0700

    feat(lat, lon, cityName, weatherImg): Add to getItem features

--- src/modules/weather-classes.js
+++ src/modules/weather-classes.js
@@ -1,3 +1,9 @@
+import { createIcon } from './dom-creation';
+import sunSrc from '../images/icons/sun.png';
+import cloudSrc from '../images/icons/cloud.png';
+import rainSrc from '../images/icons/rain.png';
+import thunderSrc from '../images/icons/thunderstorm.png';
+
 class WeatherData {
   constructor(data) {
     this.data = data;
@@ -17,6 +23,43 @@ class WeatherData {
 
   get weather() {
     return this.data.weather[0].main;
+  }
+
+  get weatherDesc() {
+    return this.data.weather[0].description;
+  }
+
+  get weatherImg() {
+    let icon;
+    switch (this.data.weather[0].main) {
+      case 'Clear':
+        icon = createIcon(sunSrc, this.data.weather[0].description);
+        break;
+      case 'Clouds':
+        icon = createIcon(cloudSrc, this.data.weather[0].description);
+        break;
+      case 'Thunderstorm':
+        icon = createIcon(thunderSrc, this.data.weather[0].description);
+        break;
+      case 'Rain':
+        icon = createIcon(rainSrc, this.data.weather[0].description);
+        break;
+      default:
+        console.log('An error has occurred in image filtering');
+    }
+    return icon;
+  }
+
+  get cityName() {
+    return this.data.name;
+  }
+
+  get lat() {
+    return this.data.coord.lat;
+  }
+
+  get lon() {
+    return this.data.coord.lon;
   }
 }
 
