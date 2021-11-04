2418999ba8e55a1fd1ec7942046167401aadc896
Edgar <ecarrillo046@gmail.com>
2021-11-03 19:46:21 -0700

    feat(eventListener): make unit switch button have functionality

--- src/modules/form-logic.js
+++ src/modules/form-logic.js
@@ -1,5 +1,6 @@
 import weatherLogic from './weather-data';
 import locationLogic from './location-logic';
+import { WeatherData } from './weather-classes';
 
 let formLogic = (() => {
   function postContent(data, locationData) {
@@ -10,14 +11,26 @@ let formLogic = (() => {
   async function postData(userInput) {
     const locationData = await locationLogic.getData(userInput);
     const coords = locationLogic.getCoords(locationData);
-    const data = await weatherLogic.getWeather(coords);
+    const units = weatherLogic.getUnits();
+    const data = await weatherLogic.getWeather(coords, units);
     postContent(data, locationData);
   }
 
   const form = document.querySelector('form');
+  let userInput = 'San Francisco, CA';
   form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const userInput = document.getElementById('address').value;
+    userInput = document.getElementById('address').value;
+    postData(userInput);
+    form.reset();
+  });
+
+  const unitContainer = document.querySelector('.unit-container');
+  unitContainer.addEventListener('click', () => {
+    const impUnit = document.querySelector('#imp-unit');
+    const metUnit = document.querySelector('#met-unit');
+    impUnit.classList.toggle('selected-unit');
+    metUnit.classList.toggle('selected-unit');
     postData(userInput);
   });
 })();
