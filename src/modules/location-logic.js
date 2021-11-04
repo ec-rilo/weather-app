6c884e91d6c820348e2246e6e58c651f55ff3c3b
Edgar <ecarrillo046@gmail.com>
2021-11-03 17:25:12 -0700

    feat(get functions): add location functions that get data

---
+++ src/modules/location-logic.js
@@ -0,0 +1,52 @@
+let locationLogic = (() => {
+  function formatLocation(userInput) {
+    userInput = userInput.replace(',', '');
+    let arrOfStr = userInput.split(' ');
+    return arrOfStr;
+  }
+
+  function getState(userInput) {
+    const arrOfStr = formatLocation(userInput);
+    const state = arrOfStr[arrOfStr.length - 1];
+    if (state.length === 2) {
+      return state;
+    } else return '';
+  }
+
+  function getCity(userInput) {
+    const arrOfStr = formatLocation(userInput);
+    arrOfStr.pop();
+    const city = arrOfStr.join(' ');
+    return city;
+  }
+
+  function getCoords(data) {
+    const coords = {
+      lat: data.results[0].geometry.location.lat,
+      lon: data.results[0].geometry.location.lat,
+    };
+    return coords;
+  }
+
+  async function getData(userInput) {
+    const apiKey = 'AIzaSyD_kDl3O6qjcGG3sNHWMnUyqHtjBEcHEJo';
+    const city = getCity(userInput);
+    const state = getState(userInput);
+
+    try {
+      const dataResponse = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${city},+${state}&key=${apiKey}`,
+        { mode: 'cors' }
+      );
+      const data = await dataResponse.json();
+
+      return data;
+    } catch (err) {
+      alert(err);
+    }
+  }
+
+  return { getData, getCoords };
+})();
+
+export default locationLogic;
