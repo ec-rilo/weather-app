51903faecbd8bbec37dd4a87fa08f8a93d84cc2c
Edgar <ecarrillo046@gmail.com>
2021-11-07 01:48:51 -0700

    feat(imports): import forecast-data.js and temp-unit-change.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import './styles/reset.css';
 import './styles/main.css';
 import './modules/weather-data';
 import './modules/form-logic';
+import './modules/forecast-data';
+import './modules/temp-unit-change';
